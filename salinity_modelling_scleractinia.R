09a0b034f269ef5e9d28fcd28362e554b9a60001
Laura Warmuth <42473137+LauraMWarmuth@users.noreply.github.com>
2023-07-07 14:19:09 +0100

    Update salinity_modelling_scleractinia.R

--- salinity_modelling_scleractinia.R
+++ salinity_modelling_scleractinia.R
@@ -47,29 +47,35 @@ write.csv(unique.genera, file = "unique_genera.csv")
 # Coral data
 occ.data <- read.csv("scleractinia_marshall_unique_new.csv")
 
+library(sp)
+library(raster)
+
 # Geographic extent of occurrences
 max.lat <- ceiling(max(occ.data$decimalLatitude))
 min.lat <- floor(min(occ.data$decimalLatitude))
 max.lon <- ceiling(max(occ.data$decimalLongitude))
 min.lon <- floor(min(occ.data$decimalLongitude))
 geographic.extent <- extent(x = c(min.lon, max.lon, min.lat, max.lat))
+geographic.extent.buffer <- extent(x = c(139, 192, -16, 37))
 
 # Environmental data
-library(sp)
-library(raster)
+# sal.126.diff.data = raster("211007T114448_210824T225103_210726_sos_CMIP6_ssp126_2071_2101_diff-0p5deg.txt")
+# sal.585.diff.data = raster("211007T114700_210824T225103_210726_sos_CMIP6_ssp585_2071_2101_diff-0p5deg.txt")
 
-sal.126.diff.data = raster("211007T114448_210824T225103_210726_sos_CMIP6_ssp126_2071_2101_diff-0p5deg.txt")
-sal.585.diff.data = raster("211007T114700_210824T225103_210726_sos_CMIP6_ssp585_2071_2101_diff-0p5deg.txt")
-
-sal.126.mean.data = raster("210927T094733_210824T225103_210726_sos_CMIP6_ssp126_2071_2101_mean-0p5deg.txt")
-sal.585.mean.data = raster("210920T124039_210824T225103_210726_sos_CMIP6_ssp585_2071_2101_mean-0p5deg.txt")
+sal.historical.mean.data = raster("230620T125922_230601T165519_230321_sos_CMIP6_historical_1985_2015_mean-0p5deg.txt")
+sal.126.mean.data = raster("230620T130023_230601T165519_230321_sos_CMIP6_ssp126_2071_2101_mean-0p5deg.txt")
+sal.370.mean.data = raster("230620T130124_230601T165519_230321_sos_CMIP6_ssp370_2071_2101_mean-0p5deg.txt")
+sal.585.mean.data = raster("230620T130225_230601T165519_230321_sos_CMIP6_ssp585_2071_2101_mean-0p5deg.txt")
 
 ## S4 method for Raster cropping
-sal.126.diff.data.cropped <- crop(sal.126.diff.data, geographic.extent)
-sal.585.diff.data.cropped <- crop(sal.585.diff.data, geographic.extent)
+# sal.126.diff.data.cropped <- crop(sal.126.diff.data, geographic.extent)
+# sal.585.diff.data.cropped <- crop(sal.585.diff.data, geographic.extent)
 
-sal.126.mean.data.cropped <- crop(sal.126.mean.data, geographic.extent)
-sal.585.mean.data.cropped <- crop(sal.585.mean.data, geographic.extent)
+sal.historical.mean.data.cropped <- crop(sal.historical.mean.data, geographic.extent.buffer)
+sal.126.mean.data.cropped <- crop(sal.126.mean.data, geographic.extent.buffer)
+sal.370.mean.data.cropped <- crop(sal.370.mean.data, geographic.extent.buffer)
+sal.585.mean.data.cropped <- crop(sal.585.mean.data, geographic.extent.buffer)
+
 
 ## Prepare occurrence data
 names(occ.data)
@@ -95,6 +101,12 @@ library(dismo)
 
 coral.presence.locations <- SpatialPoints(cbind(occ.data$decimalLongitude, occ.data$decimalLatitude))
 
+# Create RasterStack
+sal.historical.mean.data.cropped <- stack(sal.historical.mean.data.cropped)
+sal.126.mean.data.cropped <- stack(sal.126.mean.data.cropped)
+sal.370.mean.data.cropped <- stack(sal.370.mean.data.cropped)
+sal.585.mean.data.cropped <- stack(sal.585.mean.data.cropped)
+
 # MaxEnt
 library(rJava)
 maxent() # Version
@@ -107,50 +119,50 @@ occ.train <- coral.presence.locations[fold != 1, ]
 # Fitting the models
 
 #######################################
-# Salinity change data SSP scenario 126
-xm.126.diff <- maxent(sal.126.diff.data.cropped, occ.train)
-plot(sal.126.diff.data.cropped)
-
-# Plot showing importance of each variable
-plot(xm.126.diff)
-
-# Response curves
-response(xm.126.diff)
-
-# Predict to entire dataset
-xm.126.diff.pred <- predict(xm.126.diff, sal.126.diff.data.cropped)
-plot(xm.126.diff.pred)
-
-# Testing the model
-# Background data
-bg <- randomPoints(sal.126.diff.data.cropped, 10000) #background "pseudoabsences"
-
-# Simplest way to use 'evaluate'
-e1 <- evaluate(xm.126.diff, p=occ.test, a=bg, x=sal.126.diff.data.cropped)
-plot(e1, 'ROC')
-
-#######################################
-# Salinity change data SSP scenario 585
-xm.585.diff <- maxent(sal.585.diff.data.cropped, occ.train)
-plot(sal.585.diff.data.cropped)
-
-# Plot showing importance of each variable
-plot(xm.585.diff)
-
-# Response curves
-response(xm.585.diff)
-
-# Predict to entire dataset
-xm.585.diff.pred <- predict(xm.585.diff, sal.585.diff.data.cropped)
-plot(xm.585.diff.pred)
-
-# Testing the model
-# Background data
-bg <- randomPoints(sal.585.diff.data.cropped, 10000) #background "pseudoabsences"
-
-# Simplest way to use 'evaluate'
-e1 <- evaluate(xm.585.diff, p=occ.test, a=bg, x=sal.585.diff.data.cropped)
-plot(e1, 'ROC')
+# # Salinity change data SSP scenario 126
+# xm.126.diff <- maxent(sal.126.diff.data.cropped, occ.train)
+# plot(sal.126.diff.data.cropped)
+# 
+# # Plot showing importance of each variable
+# plot(xm.126.diff)
+# 
+# # Response curves
+# response(xm.126.diff)
+# 
+# # Predict to entire dataset
+# xm.126.diff.pred <- predict(xm.126.diff, sal.126.diff.data.cropped)
+# plot(xm.126.diff.pred)
+# 
+# # Testing the model
+# # Background data
+# bg <- randomPoints(sal.126.diff.data.cropped, 10000) #background "pseudoabsences"
+# 
+# # Simplest way to use 'evaluate'
+# e1 <- evaluate(xm.126.diff, p=occ.test, a=bg, x=sal.126.diff.data.cropped)
+# plot(e1, 'ROC')
+# 
+# #######################################
+# # Salinity change data SSP scenario 585
+# xm.585.diff <- maxent(sal.585.diff.data.cropped, occ.train)
+# plot(sal.585.diff.data.cropped)
+# 
+# # Plot showing importance of each variable
+# plot(xm.585.diff)
+# 
+# # Response curves
+# response(xm.585.diff)
+# 
+# # Predict to entire dataset
+# xm.585.diff.pred <- predict(xm.585.diff, sal.585.diff.data.cropped)
+# plot(xm.585.diff.pred)
+# 
+# # Testing the model
+# # Background data
+# bg <- randomPoints(sal.585.diff.data.cropped, 10000) #background "pseudoabsences"
+# 
+# # Simplest way to use 'evaluate'
+# e1 <- evaluate(xm.585.diff, p=occ.test, a=bg, x=sal.585.diff.data.cropped)
+# plot(e1, 'ROC')
 
 #####################################
 # Salinity mean data SSP scenario 126
@@ -172,7 +184,30 @@ plot(xm.126.mean.pred)
 bg <- randomPoints(sal.126.mean.data.cropped, 10000) #background "pseudoabsences"
 
 # Simplest way to use 'evaluate'
-e1 <- evaluate(xm.126.mean.pred, p=occ.test, a=bg, x=sal.126.mean.data.cropped)
+e1 <- evaluate(xm.126.mean, p=occ.test, a=bg, x=sal.126.mean.data.cropped)
+plot(e1, 'ROC')
+
+#####################################
+# Salinity mean data SSP scenario 370
+xm.370.mean <- maxent(sal.370.mean.data.cropped, occ.train)
+plot(sal.370.mean.data.cropped)
+
+# Plot showing importance of each variable
+plot(xm.370.mean)
+
+# Response curves
+response(xm.370.mean)
+
+# Predict to entire dataset
+xm.370.mean.pred <- predict(xm.370.mean, sal.370.mean.data.cropped)
+plot(xm.370.mean.pred)
+
+# Testing the model
+# Background data
+bg <- randomPoints(sal.370.mean.data.cropped, 10000) #background "pseudoabsences"
+
+# Simplest way to use 'evaluate'
+e1 <- evaluate(xm.370.mean, p=occ.test, a=bg, x=sal.370.mean.data.cropped)
 plot(e1, 'ROC')
 
 #####################################
@@ -202,76 +237,144 @@ plot(e1, 'ROC')
 ###########
 ## Plotting
 library(ggplot2)
+library(mapdata)
+
 
 # Define colour palette
 cols = colorRampPalette(c("#C13127","#EDF0C0","#5E85B8"))
 
 ####################################################################
-# Plot predictions for salinity change data SSP scenario 126 (Fig 4)
-png(file="sos_change_126_marshall_prediction.png", width = 1230, height = 1200, res = 300)
-
-#postscript("sos_change_126_marshall_prediction.eps", width = 1230, height = 1200)
-
-plot(xm.126.diff.pred, col = cols(100), xaxt = "n", yaxt = "n")
-
-map('worldHires', fill=FALSE, add=TRUE)
-map('worldHires', fill=FALSE, add=TRUE)
-map('worldHires', fill=FALSE, add=TRUE)
-
-ylab="Latitude (Degrees)"
-axis(side=1,at=c(140,150,160,170,180,190),labels=c("140E","150E","160E","170E","180E","190E"))
-axis(side=2,at=c(-10,0,10,20,30),labels=c("10S","0","10N","20N","30N"))
-legend(cex=0.8)
-cex.axis=0.8
-mtext("SSP1-2.6", side=3, font =2, adj=1, cex=0.8)
-
-points(occ.data$decimalLongitude, occ.data$decimalLatitude, pch = 21, cex = 0.8, bg = "darkgrey", col = "black")
-
-dev.off()
+# # Plot predictions for salinity change data SSP scenario 126 (Fig 4b)
+# png(file="sos_change_126_marshall_prediction.png", width = 1230, height = 1200, res = 300)
+# 
+# #postscript("sos_change_126_marshall_prediction.eps", width = 1230, height = 1200)
+# 
+# plot(xm.126.diff.pred, col = cols(100), xaxt = "n", yaxt = "n")
+# 
+# map('worldHires', fill=FALSE, add=TRUE)
+# map('worldHires', fill=FALSE, add=TRUE)
+# map('worldHires', fill=FALSE, add=TRUE)
+# 
+# ylab="Latitude (Degrees)"
+# axis(side=1,at=c(140,150,160,170,180,190),labels=c("140E","150E","160E","170E","180E","190E"))
+# axis(side=2,at=c(-10,0,10,20,30),labels=c("10S","0","10N","20N","30N"))
+# legend(cex=0.8)
+# cex.axis=0.8
+# mtext("SSP1-2.6", side=3, font =2, adj=1, cex=0.8)
+# 
+# points(occ.data$decimalLongitude, occ.data$decimalLatitude, pch = 21, cex = 0.8, bg = "darkgrey", col = "black")
+# 
+# dev.off()
 
 ####################################################################
-# Plot predictions for salinity change data SSP scenario 585 (Fig 4)
-
-png(file="sos_change_585_marshall_prediction.png", width = 1230, height = 1200, res = 300)
-
-#postscript("sos_change_585_marshall_prediction.eps", width = 1230, height = 1200)
-
-plot(xm.585.diff.pred, col = cols(100), xaxt = "n", yaxt = "n")
-
-map('worldHires', fill=FALSE, add=TRUE)
-map('worldHires', fill=FALSE, add=TRUE)
-map('worldHires', fill=FALSE, add=TRUE)
-
-xlab="Longitude (Degrees)"
-axis(side=1,at=c(140,150,160,170,180,190),labels=c("140E","150E","160E","170E","180E","190E"))
-# axis(side=2,at=c(-10,0,10,20,30),labels=c("10S","0","10N","20N","30N"))
-legend(cex=0.8)
-cex.axis=0.8
-mtext("SSP5-8.5", side=3, font =2, adj=1, cex=0.8)
-
-points(occ.data$decimalLongitude, occ.data$decimalLatitude, pch = 21, cex = 0.8, bg = "darkgrey", col = "black")
-
-dev.off()
+# # Plot predictions for salinity change data SSP scenario 585 (Fig 4b)
+# 
+# png(file="sos_change_585_marshall_prediction.png", width = 1230, height = 1200, res = 300)
+# 
+# #postscript("sos_change_585_marshall_prediction.eps", width = 1230, height = 1200)
+# 
+# plot(xm.585.diff.pred, col = cols(100), xaxt = "n", yaxt = "n")
+# 
+# map('worldHires', fill=FALSE, add=TRUE)
+# map('worldHires', fill=FALSE, add=TRUE)
+# map('worldHires', fill=FALSE, add=TRUE)
+# 
+# xlab="Longitude (Degrees)"
+# axis(side=1,at=c(140,150,160,170,180,190),labels=c("140E","150E","160E","170E","180E","190E"))
+# # axis(side=2,at=c(-10,0,10,20,30),labels=c("10S","0","10N","20N","30N"))
+# legend(cex=0.8)
+# cex.axis=0.8
+# mtext("SSP5-8.5", side=3, font =2, adj=1, cex=0.8)
+# 
+# points(occ.data$decimalLongitude, occ.data$decimalLatitude, pch = 21, cex = 0.8, bg = "darkgrey", col = "black")
+# 
+# dev.off()
 
 ###################################################################
 # Plot predictions for salinity mean data SSP scenario 126 (Fig S2)
 
+# PNG File
 png(file="sos_mean_126_marshall_prediction.png", width = 1230, height = 1200, res = 300)
 
-#postscript("sos_mean_126_marshall_prediction.eps", width = 1230, height = 1200)
-
-plot(xm.126.mean.pred, col = cols(100),xaxt = "n", yaxt = "n")
+plot(xm.126.mean.pred, col = cols(100), xaxt = "n", yaxt = "n")
 
 map('worldHires', fill=FALSE, add=TRUE)
 map('worldHires', fill=FALSE, add=TRUE)
 map('worldHires', fill=FALSE, add=TRUE)
 
-ylab="Latitude (Degrees)"
-axis(side=1,at=c(140,150,160,170,180,190),labels=c("140E","150E","160E","170E","180E","190E"))
+# ylab="Latitude (Degrees)"
+# xlab="Longitude (Degrees)"
+axis(side=1,at=c(140,150,160,170,180,190),labels=c("140E","150E","160E","170E","180","170W"))
 axis(side=2,at=c(-10,0,10,20,30),labels=c("10S","0","10N","20N","30N"))
-legend(cex=0.8)
+# legend(cex=0.8)
 cex.axis=0.8
 mtext("SSP1-2.6", side=3, font =2, adj=1, cex=0.8)
+
+points(occ.data$decimalLongitude, occ.data$decimalLatitude, pch = 21, cex = 0.8, bg = "darkgrey", col = "black")
+
+dev.off()
+
+# EPS File
+postscript("sos_mean_126_marshall_prediction.eps", width = 1230, height = 1200)
+
+plot(xm.126.mean.pred, col = cols(100), xaxt = "n", yaxt = "n")
+
+map('worldHires', fill=FALSE, add=TRUE)
+map('worldHires', fill=FALSE, add=TRUE)
+map('worldHires', fill=FALSE, add=TRUE)
+
+# ylab="Latitude (Degrees)"
+# xlab="Longitude (Degrees)"
+axis(side=1,at=c(140,150,160,170,180,190),labels=c("140E","150E","160E","170E","180","170W"))
+axis(side=2,at=c(-10,0,10,20,30),labels=c("10S","0","10N","20N","30N"))
+# legend(cex=0.8)
+cex.axis=0.8
+mtext("SSP1-2.6", side=3, font =2, adj=1, cex=0.8)
+
+points(occ.data$decimalLongitude, occ.data$decimalLatitude, pch = 21, cex = 0.8, bg = "darkgrey", col = "black")
+
+dev.off()
+
+##############################################################################################
+# Plot predictions for salinity mean data SSP scenario 370 (this scenario is not yet in a Fig)
+
+# PNG File
+png(file="sos_mean_370_marshall_prediction.png", width = 1230, height = 1200, res = 300)
+
+plot(xm.370.mean.pred, col = cols(100),xaxt = "n", yaxt = "n")
+
+map('worldHires', fill=FALSE, add=TRUE)
+map('worldHires', fill=FALSE, add=TRUE)
+map('worldHires', fill=FALSE, add=TRUE)
+
+# ylab="Latitude (Degrees)"
+# xlab="Longitude (Degrees)"
+axis(side=1,at=c(140,150,160,170,180,190),labels=c("140E","150E","160E","170E","180","170W"))
+axis(side=2,at=c(-10,0,10,20,30),labels=c("10S","0","10N","20N","30N"))
+# legend(cex=0.8)
+cex.axis=0.8
+mtext("SSP3-7.0", side=3, font =2, adj=1, cex=0.8)
+
+points(occ.data$decimalLongitude, occ.data$decimalLatitude, pch = 21, cex = 0.8, bg = "darkgrey", col = "black")
+
+dev.off()
+
+# EPS File
+postscript("sos_mean_370_marshall_prediction.eps", width = 1230, height = 1200)
+
+plot(xm.370.mean.pred, col = cols(100),xaxt = "n", yaxt = "n")
+
+map('worldHires', fill=FALSE, add=TRUE)
+map('worldHires', fill=FALSE, add=TRUE)
+map('worldHires', fill=FALSE, add=TRUE)
+
+# ylab="Latitude (Degrees)"
+# xlab="Longitude (Degrees)"
+axis(side=1,at=c(140,150,160,170,180,190),labels=c("140E","150E","160E","170E","180","170W"))
+axis(side=2,at=c(-10,0,10,20,30),labels=c("10S","0","10N","20N","30N"))
+# legend(cex=0.8)
+cex.axis=0.8
+mtext("SSP3-7.0", side=3, font =2, adj=1, cex=0.8)
 
 points(occ.data$decimalLongitude, occ.data$decimalLatitude, pch = 21, cex = 0.8, bg = "darkgrey", col = "black")
 
@@ -280,9 +383,8 @@ dev.off()
 ###################################################################
 # Plot predictions for salinity mean data SSP scenario 585 (Fig S2)
 
+# PNG File
 png(file="sos_mean_585_marshall_prediction.png", width = 1230, height = 1200, res = 300)
-
-#postscript("sos_mean_585_marshall_prediction.eps", width = 1230, height = 1200)
 
 plot(xm.585.mean.pred, col = cols(100),xaxt = "n", yaxt = "n")
 
@@ -290,10 +392,32 @@ map('worldHires', fill=FALSE, add=TRUE)
 map('worldHires', fill=FALSE, add=TRUE)
 map('worldHires', fill=FALSE, add=TRUE)
 
-xlab="Longitude (Degrees)"
-axis(side=1,at=c(140,150,160,170,180,190),labels=c("140E","150E","160E","170E","180E","190E"))
-# axis(side=2,at=c(-10,0,10,20,30),labels=c("10S","0","10N","20N","30N"))
-legend(cex=0.8)
+# ylab="Latitude (Degrees)"
+# xlab="Longitude (Degrees)"
+axis(side=1,at=c(140,150,160,170,180,190),labels=c("140E","150E","160E","170E","180","170W"))
+axis(side=2,at=c(-10,0,10,20,30),labels=c("10S","0","10N","20N","30N"))
+# legend(cex=0.8)
+cex.axis=0.8
+mtext("SSP5-8.5", side=3, font =2, adj=1, cex=0.8)
+
+points(occ.data$decimalLongitude, occ.data$decimalLatitude, pch = 21, cex = 0.8, bg = "darkgrey", col = "black")
+
+dev.off()
+
+# EPS File
+postscript("sos_mean_585_marshall_prediction.eps", width = 1230, height = 1200)
+
+plot(xm.585.mean.pred, col = cols(100),xaxt = "n", yaxt = "n")
+
+map('worldHires', fill=FALSE, add=TRUE)
+map('worldHires', fill=FALSE, add=TRUE)
+map('worldHires', fill=FALSE, add=TRUE)
+
+# ylab="Latitude (Degrees)"
+# xlab="Longitude (Degrees)"
+axis(side=1,at=c(140,150,160,170,180,190),labels=c("140E","150E","160E","170E","180","170W"))
+axis(side=2,at=c(-10,0,10,20,30),labels=c("10S","0","10N","20N","30N"))
+# legend(cex=0.8)
 cex.axis=0.8
 mtext("SSP5-8.5", side=3, font =2, adj=1, cex=0.8)
 
